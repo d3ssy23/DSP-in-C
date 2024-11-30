4b3bf4c45c33c00f414bd0955a9412d49a22114a
d3ssy23 <dilieva.audio@gmail.com>
2024-11-30 17:54:50 +0200

    comm

--- DFT/main.c
+++ DFT/main.c
@@ -1,26 +1,33 @@
+#define _USE_MATH_DEFINES 
+
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #define SIG_LEN 320
+#define PI 3.14159265359
 
 extern double InputSignal_f32_1kHz_15kHz[SIG_LEN];
 double Output_REX[SIG_LEN/2];
 double Output_IMX[SIG_LEN/2];
+double Output_MAG[SIG_LEN/2];
+
 void  dft(double *sig_src_arr, double *sig_dest_rex, double *sig_dest_imx, int len);
+void dft_magnetude(double *sig_dest_mag);
 
 
 int main(){
 
-    FILE *fptr1, *fptr2, *fptr3;
+    FILE *fptr1, *fptr2, *fptr3, *fptr4;
 
     dft(&InputSignal_f32_1kHz_15kHz[0], &Output_REX[0], &Output_IMX[0], SIG_LEN);
+    dft_magnetude(&Output_MAG[0]);
 
 
     fptr1 = fopen("input_signal.dat", "w");
     fptr2 = fopen("output_rex.dat", "w");       //also .txt format can be used for plotting with matlab and gnuplot
     fptr3 = fopen("output_imx.dat", "w");
-
+    fptr4 = fopen("dft_magnetude.dat" , "w");
 
     for(int i = 0; i<SIG_LEN;i++)
     {
@@ -37,9 +44,14 @@ int main(){
     for(int i = 0; i<SIG_LEN/2;i++)
     {
         fprintf(fptr3,"\n%f", Output_IMX[i]);
-
     }
     fclose(fptr3);
+
+    for(int i = 0; i < SIG_LEN/2; i++)
+    {
+        fprintf(fptr4, "\n%f", Output_MAG[i]);
+    }
+    fclose(fptr4);
 
     return 0;
 
@@ -48,8 +60,6 @@ int main(){
 void  dft(double *sig_src_arr, double *sig_dest_rex, double *sig_dest_imx, int len)
 {
     int i,j,k;
-    double PI = 3.14159265359;
-
     for(j = 0; j< (len/2);j++)   //the signal lenght is divided by two since it has real and imaginary part both equal to N/2
     {
         sig_dest_imx[j] = 0;
@@ -60,8 +70,17 @@ void  dft(double *sig_src_arr, double *sig_dest_rex, double *sig_dest_imx, int l
     {
          for (i=0;i<len;i++)
          {
-            sig_dest_rex[k] = sig_dest_rex[k] + sig_src_arr[i]* cos(2*PI*k*i/len);       //calculation of the real part
-            sig_dest_imx[k] = sig_dest_imx[k] - sig_src_arr[i]* sin(2*PI*k*i/len);       //calculation of the imaginary part
+            sig_dest_rex[k] = sig_dest_rex[k] + sig_src_arr[i]*cos(2*PI*k*i/len);       //calculation of the real part
+            sig_dest_imx[k] = sig_dest_imx[k] - sig_src_arr[i]*sin(2*PI*k*i/len);       //calculation of the imaginary part
          }
+    }
+}
+
+void dft_magnetude(double *sig_dest_mag)
+{
+    int k;
+    for(k = 0; k < SIG_LEN/2; k++)
+    {
+        sig_dest_mag[k] = sqrt(pow(Output_REX[k],2)+ pow(Output_IMX[k],2));
     }
 }
